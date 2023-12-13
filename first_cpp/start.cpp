bb4f78d1375f860ec13008b6efbfee98cabafb2f
LIBCSYS <libcsystems@gmail.com>
2023-12-13 10:43:13 -0500

    Add project files.

---
+++ first_cpp/start.cpp
@@ -0,0 +1,14 @@
+/* -------------------------------------------------------------------------------------------------
+
+start.cpp written by joel winter 2023
+Learning cpp for Unreal Engine 5
+
+This is a learning process.  will build on it from here.
+ -------------------------------------------------------------------------------------------------*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World from C++.\nI once recieved a response from Bjorn Stroustrup \nI emailed him stating, just seeing if I'll get a reply, to which I did.";
+    return 0;
+}
